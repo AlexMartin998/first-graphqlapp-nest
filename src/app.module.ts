6991b794386b2617ac991d03f236d7921c505c0b
Alex Martin <alextravc.342@gmail.com>
2023-01-27 21:31:52 -0500

    [ADD]: typeorm and pg dependencies added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ItemsModule } from './items/items.module';
 import { ConfigModule } from '@nestjs/config';
 import { EnvConfiguration } from './config/app.config';
@@ -24,8 +25,22 @@ import { EnvConfiguration } from './config/app.config';
 
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+
     ItemsModule,
   ],
+
   controllers: [],
   providers: [],
 })
