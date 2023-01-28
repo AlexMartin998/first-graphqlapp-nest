6139c7e13cfde99966d25a62b50eee7159649d83
Alex Martin <alextravc.342@gmail.com>
2023-01-28 07:54:35 -0500

    [ADD]: get item by id added

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateItemInput, UpdateItemInput } from './dto/inputs';
@@ -22,8 +22,11 @@ export class ItemsService {
     return this.itemsRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} item`;
+  async findOne(id: string): Promise<Item> {
+    const item = await this.itemsRepository.findOneBy({ id });
+    if (!item) throw new NotFoundException(`Item with id: '${id}' not found`);
+
+    return item;
   }
 
   update(id: number, updateItemInput: UpdateItemInput) {
